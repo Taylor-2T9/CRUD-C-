f51640df09abcb38804294e2cc9126da44c0057f
Taylor-2t9 <ftayloros29@gmail.com>
2022-06-26 12:54:44 -0300

    CRUD Pronto

---
+++ Crud/Models/BancoDeDados.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Crud.Models
+{
+    public class BancoDeDados : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;database=Crud;Integrated Security=True");
+        }
+    }
+}
